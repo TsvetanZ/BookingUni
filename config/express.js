d4a04806c3733edc136abca2f79e1b89feac5afc
Tsvetan Zahariev <102287382+TsvetanZ@users.noreply.github.com>
2022-10-17 00:30:17 +0300

    create first step structure

---
+++ config/express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const handlebars = require('express-handlebars')
+
+
+module.exports = (app) => {
+     const hbs = handlebars.create({
+        extname: 'hbs'
+     });
+
+     app.engine('.hbs', hbs.engine());
+     app.set('view engine', '.hbs');
+};
